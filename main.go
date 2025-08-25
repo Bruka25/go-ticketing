d13544bd56c908d78d0ff24e2bd0ce8969effaf0
Bruka25 <bkteshome862@gmail.com>
2025-08-25 18:20:30 +0300

    ticketing app update with an array to store usernames

--- main.go
+++ main.go
@@ -10,11 +10,15 @@ fmt.Printf("conferenceTickets is %T, confName is %T, remainingTickets is %T\n", 
 fmt.Printf("welcome to %v booking application\n", confName)
 fmt.Printf("we have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 fmt.Println("Get your tickets here to attend")
+
+    
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
 	// ask a user for their name
+	var bookings [50]string
+	
 
 	//fmt.Println(remainingTickets)
 	//fmt.Println(&remainingTickets)
@@ -32,9 +36,13 @@ fmt.Println("Get your tickets here to attend")
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
 	//userName = "Bruka"
 	//userTickets = 2
-
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("The type of array: %T\n", bookings)
+	fmt.Printf("Array Length: %v\n", len(bookings))
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confName)
 }
