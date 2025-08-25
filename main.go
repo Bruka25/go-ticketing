b8c5792e142024c1cc686f6d8af9f5164baf72b5
Bruka25 <bkteshome862@gmail.com>
2025-08-25 17:04:22 +0300

    variable types and references with a user name

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ func main() {
 var confName = "Go conference"
 const conferenceTickets = 50
 var remainingTickets = 50
-//fmt.Println("welcome to our booking application")
-//fmt.Println("we are happy to have you here") 
+fmt.Printf("conferenceTickets is %T, confName is %T, remainingTickets is %T\n", conferenceTickets, confName, remainingTickets)
 fmt.Printf("welcome to %v booking application\n", confName)
 fmt.Printf("we have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 fmt.Println("Get your tickets here to attend")
-} 
+	var userName string
+	var userTickets int
+	// ask a user for their name
+
+	userName = "Bruka"
+	userTickets = 2
+
+	fmt.Printf("user %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Thank you for booking, see you there!")
+}
